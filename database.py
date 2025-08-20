ba936cced4028ce88810ef641cba2a43663ee62e
Riccardo Buscicchio <chionatan@gmail.com>
2025-08-20 11:10:13 +0200

    Update database.py

--- database.py
+++ database.py
@@ -129,7 +129,7 @@ if published:
     papers['published']['data'].append({
     "title":    "A test for LISA foreground Gaussianity and stationarity. I. Galactic white-dwarf binaries",
     "author":   "R. Buscicchio, A. Klein, V. Korol, F. Di Renzo, C.J. Moore, D. Gerosa, A. Carzaniga",
-    "journal":  "Eur.~Phys.~J. C 85, (2025) 887",
+    "journal":  "\epjc 85, (2025) 887",
     "link":     "https://doi.org/10.1140/epjc/s10052-025-14616-w",
     "arxiv":    "arXiv:2410.08263 [astro-ph.HE]",
     "ads":      "2024arXiv241008263B",
